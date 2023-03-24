99b661d53a971bef1f32356c08f8851022b6b404
Santiago <gosanti988@gmail.com>
2023-03-24 01:34:16 -0500

    rpoyecto base de gif

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,14 @@
+// comando para crear un componente de forma rápida rafc+tab
+import React from 'react'
+
+export const GifExpertApp = () => {
+  return (
+    <>
+    {/* titulo de la aplicacion */}
+    <h1>GifExpertApp</h1>
+    {/* Input */}
+    {/* Listado de Gif */}
+        {/* Gif Item */}
+    </>
+  )
+}
